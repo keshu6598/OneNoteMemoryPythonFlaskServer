5b6fdad3d4692d78a74bba2bea55fea60e35ac47
Kanishk Khandelwal <kanishkk@microsoft.com>
2023-02-15 11:49:30 +0530

    initialized the server

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import os
+import openai
+
+openai.api_key = "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"
+
+
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
 app = Flask(__name__)
 
 
@@ -13,6 +19,26 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/rephrase', methods=['POST'])
+def rephrase():
+    request_body = request.get_json()
+    rephrase_text = request_body['text']
+    prompt_text = "Hi ChatGPT, rephrase this text for me \' " + rephrase_text + " \' "
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt_text,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    print('Response from OpenAI: ', response)
+    data = {
+        "rephraseText" : response,
+    }
+    return jsonify(data)
+
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
@@ -23,6 +49,7 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
+
 
 
 if __name__ == '__main__':
